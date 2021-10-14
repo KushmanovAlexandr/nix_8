6140ebe60254ded1938493c69e3b2a6f0f0a03a2
Alexandr Kushmanov <kushmanov555@gmail.com>
2021-10-14 23:18:49 +0300

    Start hw_2_strings

---
+++ hw_2_strings/src/main/java/ua/com/alevel/reversestring/AllReverseAndReverseOptionsWithIndexIndication.java
@@ -0,0 +1,10 @@
+package ua.com.alevel.reversestring;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+public class AllReverseAndReverseOptionsWithIndexIndication {
+    public void run(BufferedReader reader) throws IOException {
+        System.out.println("Enter your string:");
+    }
+}
