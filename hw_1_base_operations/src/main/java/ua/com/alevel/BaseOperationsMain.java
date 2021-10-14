92ddbde3ec4d68c97e00f0bdba86529982f4bcac
Alexandr Kushmanov <kushmanov555@gmail.com>
2021-10-14 23:18:49 +0300

    test

--- hw_1_base_operations/src/main/java/ua/com/alevel/BaseOperationsMain.java
+++ hw_1_base_operations/src/main/java/ua/com/alevel/BaseOperationsMain.java
@@ -4,6 +4,7 @@ public class BaseOperationsMain {
 
     public static void main(String[] args) {
         System.out.println("BaseOperationsMain.main");
+        System.out.println("BaseOperationsMain.main");
         ProgramRun.run();
     }
 }
