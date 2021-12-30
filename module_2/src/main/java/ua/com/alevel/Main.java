72ab2127c7d57b21130f0596e99355df67c12270
egorfuntusov <funtushan@gmail.com>
2021-12-30 07:19:28 +0200

    add review

--- module_2/src/main/java/ua/com/alevel/Main.java
+++ module_2/src/main/java/ua/com/alevel/Main.java
@@ -3,9 +3,8 @@ package ua.com.alevel;
 public class Main {
 
     public static void main(String[] args){
-        ModuleSecond.firstTask(".\\input1.txt", ".\\output1.txt");
-        ModuleSecond.secondTask(".\\input2.txt", ".\\output2.txt");
-        ModuleSecond.thirdTask(".\\input3.txt", ".\\output3.txt");
+        ModuleSecond.firstTask("input1.txt", "output1.txt");
+        ModuleSecond.secondTask("input2.txt", "output2.txt");
+        ModuleSecond.thirdTask("input3.txt", "output3.txt");
     }
-
 }
